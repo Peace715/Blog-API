efc5b9e027e1bfcf9133e8c9f575f3c7960545c0
Peace715 <peaceotoide78@gmail.com>
2026-05-22 06:57:01 +0100

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login } = require('../controllers/userController');
-
-router.post('/signup', signup);
-console.log('Signup response:', res.body )
-router.post('/login', login);
-
-module.exports = router;
